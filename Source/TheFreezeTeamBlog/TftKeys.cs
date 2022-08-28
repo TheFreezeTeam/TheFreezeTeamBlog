f2d4a361c1acce936c0d3fd0e61d8680f836827d
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-08-28 04:29:10 +0000

    add AuthorImage TftKey

--- Source/TheFreezeTeamBlog/TftKeys.cs
+++ Source/TheFreezeTeamBlog/TftKeys.cs
@@ -5,5 +5,6 @@
     public const string SiteTitle = nameof(SiteTitle);
     public const string PageTitle = nameof(PageTitle);
     public const string PostImageUrl = nameof(PostImageUrl);
+    public const string AuthorImage = nameof(AuthorImage);
   }
 }
